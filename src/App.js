1221534bec1b06394db41e6651b9c618b46be25e
Joshua Bubune <122805926+jbagbeke@users.noreply.github.com>
2024-05-23 11:13:15 +0000

    Update 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h3", {}, "Havenese"),
+    ]);
+}
+
 const App = () => {
     return React.createElement(
       "div",
       {},
-      React.createElement("h1", {}, "Adopt Me Now")
+      [ 
+        React.createElement("h1", {}, "Adopt Me Now"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet),
+      ]
     )
   };
 
